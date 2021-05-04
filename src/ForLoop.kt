ef70a9e327c2b45129c4355ebfe63a6a48663bff
Marco Barreto <kinhobarreto09@gmail.com>
2021-05-03 22:33:25 -0300

    Estrutura FOR

---
+++ src/ForLoop.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val str = "Marco Barreto"
+    for (c in str){
+        print(" $c ")
+    }
+
+    for (i in 1..100 step 2){
+        print("$i ")
+    }
+
+    for (j in 100 downTo 1){
+        print("$j ")
+    }
+}
