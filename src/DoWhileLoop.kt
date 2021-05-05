780722308c3aab909cff8198625c07ad64fa3e8c
Marco Barreto <kinhobarreto09@gmail.com>
2021-05-04 21:30:56 -0300

    While e doWhile loops

---
+++ src/DoWhileLoop.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+
+    do {
+        println("Deve ser printado apenas uma vez")
+    } while(false)
+
+    var i:Int = 0
+
+    do {
+        println(i)
+        i+=2
+    } while (i<10)
+}
---
+++ src/WhileLoop.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    var index: Int = 0
+    while (index <= 10){
+        print("$index \n")
+        index++
+
+        if (index == 50){
+            break
+        }
+    }
+
+}
