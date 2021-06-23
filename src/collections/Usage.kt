91a72acd2ea3eec72c25f0360d0d0d811e9e9038
marcosouzabarreto <kinhobarreto09@github.com>
2021-06-23 18:39:00 -0300

    Collection Usages

---
+++ src/collections/Usage.kt
@@ -0,0 +1,58 @@
+package collections
+
+data class Receita(val nome: String, val calorias: Int, val ingredientes: List<Ingredientes> = listOf())
+data class Ingredientes(val nome: String, val quantidade: Int)
+
+fun main() {
+
+    // Cria lista de dados
+    val data = listOf(
+            Receita(
+                    "Lasanha", 1200,
+                    listOf(
+                            Ingredientes("Farinha", 1),
+                            Ingredientes("Presunto", 5),
+                            Ingredientes("Queijo", 10),
+                            Ingredientes("Molho de tomate", 2),
+                            Ingredientes("Manjerição", 3)
+                    )
+            ),
+            Receita("Panqueca", 500),
+            Receita("Omelete", 200),
+            Receita("Parmegiana", 700),
+            Receita("Sopa de feijão", 300),
+            Receita(
+                    "Hamburguer", 2000,
+                    listOf(
+                            Ingredientes("Pão", 1),
+                            Ingredientes("Hamburguer", 3),
+                            Ingredientes("Queijo", 1),
+                            Ingredientes("Catupiry", 1),
+                            Ingredientes("Bacon", 3),
+                            Ingredientes("Alface", 1),
+                            Ingredientes("Tomate", 1)
+                    )
+            )
+    )
+
+    //Tenho receitas na lista?
+    println("Tenho receitas? ${if (data.any()) "sim" else "nao"}")
+
+    //Quantas receitas tenho?
+    println("Tenho ${data.size} receitas")
+
+    //Primeira receita
+    println("A primeira receita e ${data.first().nome}, e a ultima é ${data.last().nome}")
+
+    //Qual a soma de calorias
+    val calorias = data.sumOf { it.calorias }
+    println("As receitas tem $calorias calorias")
+
+    //Primeiras 2 receitas
+    val firstTwo = data.take(2)
+
+    for (x in firstTwo.withIndex()) {
+       println("A posicao ${x.index + 1} é ${x.value.nome}")
+    }
+
+}
